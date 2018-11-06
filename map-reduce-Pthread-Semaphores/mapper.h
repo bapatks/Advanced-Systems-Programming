edf07488ab762fbcf6acb0ba1ccf5770f92f4b4c
Kaustubh Bapat <kaustubh4694@gmail.com>
2018-11-06 13:07:06 -0500

    Add files via upload

---
+++ map-reduce-Pthread-Semaphores/mapper.h
@@ -0,0 +1,45 @@
+//mapper.h file
+#include<pthread.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdbool.h>
+#include<semaphore.h>
+
+#ifndef mapper_h_
+#define mapper_h_
+
+int buffer_size;
+int user_num;
+char fname[20]; //file size cannot be more than 9 characters long (including file extension)
+
+struct tuple
+{
+    char userID[5];
+    char topic[16];
+    int weight;
+};
+
+struct buff_ctrl	
+{
+	struct tuple* buffer_ptr;
+	sem_t lock;
+	sem_t full;
+	sem_t empty;
+	int in;
+	int out;
+	int numItems;
+	int numProduced;
+	int numConsumed;
+	int done;
+	/*
+	myID is just a number between 0 to user_num indicating/assigning the thread number to reducer threads.
+	Does not have any relation with userID assigned to this member of buff_ctrl 
+	*/
+	int myID;
+};
+
+void* mapper(void *);
+ 
+#endif
