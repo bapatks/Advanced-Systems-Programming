e3ea6b1e32d9a3116c8153d7f0b7b2b506274186
Kaustubh Bapat <kaustubh4694@gmail.com>
2018-11-06 12:50:25 -0500

    Update combiner.c

--- map-reduce-Pthreads/combiner.c
+++ map-reduce-Pthreads/combiner.c
@@ -31,7 +31,7 @@ int main(int argc, char* argv[])
     	   buffer of   ||
     	   each reducer||
     	   thread      ||
-                       \/
+                      \/
     	 =>        [ struct tuple0 , struct tuple1  , struct tuple2................., struct tuple9]....the same for every ptr in buff
 
     */
